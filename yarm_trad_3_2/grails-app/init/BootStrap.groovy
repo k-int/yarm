681bd676e78abb3f6138052f9fc02ca1e0a51bb4
Ian Ibbotson <ianibbo@gmail.com>
2016-10-01 21:01:57 +0100

    updated

--- yarm_trad_3_2/grails-app/init/BootStrap.groovy
+++ yarm_trad_3_2/grails-app/init/BootStrap.groovy
@@ -1,6 +1,31 @@
+package com.k_int.yarm
+
 class BootStrap {
 
     def init = { servletContext ->
+
+      def user_role = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
+
+      log.debug("Create admin user...");
+      def adminUser = User.findByUsername('admin')
+      if ( ! adminUser ) {
+        adminUser = new User(
+            username: 'admin',
+            password: 'admin',
+            display: 'Admin',
+            email: 'admin@localhost',
+            enabled: true).save(failOnError: true)
+      }
+
+      // Make sure admin user has all the system roles.
+      [user_role].each { role ->
+        if (!adminUser.authorities.contains(role)) {
+          UserRole.create adminUser, role
+        }
+      }
+
+
+
     }
     def destroy = {
     }
