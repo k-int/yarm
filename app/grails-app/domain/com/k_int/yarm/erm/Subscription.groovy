361734b5a8913dffdc3746af5853ed8eda0e1f28
Ian Ibbotson <ian.ibbotson@gmail.com>
2016-11-08 16:21:24 +0000

    Clean out app

--- app/grails-app/domain/com/k_int/yarm/erm/Subscription.groovy
+++
@@ -1,7 +0,0 @@
-package com.k_int.yarm.erm
-
-class Subscription {
-
-    static constraints = {
-    }
-}
--- app/grails-app/domain/com/k_int/yarm/kb/Package.groovy
+++
@@ -1,7 +0,0 @@
-package com.k_int.yarm.kb
-
-class Package {
-
-    static constraints = {
-    }
-}
