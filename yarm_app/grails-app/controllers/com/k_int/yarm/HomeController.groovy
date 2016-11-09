65380498b935c10c657f5cd03b1e81052f0037b2
Ian Ibbotson <ian.ibbotson@k-int.com>
2016-11-09 15:53:26 +0000

    updated

--- yarm_app/grails-app/controllers/com/k_int/yarm/HomeController.groovy
+++ yarm_app/grails-app/controllers/com/k_int/yarm/HomeController.groovy
@@ -1,8 +1,13 @@
 package com.k_int.yarm
 
+import static grails.gorm.multitenancy.Tenants.*
+
 class HomeController {
 
   def index() { 
     log.debug("Home::Index");
+    withCurrent {
+      log.debug("Got current tenant ${grails.gorm.multitenancy.Tenants.currentId}");
+    }
   }
 }
