cd3a8017359d4ae6ed29d9d01b9251fc15cc52e3
Ian Ibbotson <ian.ibbotson@k-int.com>
2016-11-09 09:49:32 +0000

    Created dm plugin and app

---
+++ yarm_app/grails-app/init/yarm_app/BootStrap.groovy
@@ -0,0 +1,9 @@
+package yarm_app
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
---
+++ yarm_dm/grails-app/init/yarm_dm/BootStrap.groovy
@@ -0,0 +1,9 @@
+package yarm_dm
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
