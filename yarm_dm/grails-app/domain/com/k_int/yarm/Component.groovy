2ae0c0f91a16c93bdcfcd217258ac496953e397b
Ian Ibbotson <ian.ibbotson@k-int.com>
2016-11-09 10:01:17 +0000

    Added component

---
+++ yarm_dm/grails-app/domain/com/k_int/yarm/Component.groovy
@@ -0,0 +1,25 @@
+package com.k_int.yarm
+
+public class Component {
+  
+  String name
+  String normname
+  String shortcode
+
+  static mapping = {
+    tablePerHierarchy false
+    id column:'c_id'
+    version column:'c_version'
+    name column:'c_name', type:'text'
+    normname column:'c_normname', type:'text', index:'c_normname_idx'
+    shortcode column:'c_shortcode', index:'c_shortcode'
+  }
+
+
+  static constraints = {
+             name (nullable:true, blank:false, maxSize:2048)
+         normname (nullable:true, blank:false, maxSize:2048)
+        shortcode (nullable:true, blank:false, maxSize:128)
+  }
+
+}
