43587019e6fe14f07e87e374993dccb2fba36d7d
Ian Ibbotson <ian.ibbotson@gmail.com>
2016-11-15 17:32:39 +0000

    More work

--- yarm_dm/grails-app/domain/com/k_int/yarm/Component.groovy
+++ yarm_dm/grails-app/domain/com/k_int/yarm/Component.groovy
@@ -1,6 +1,6 @@
 package com.k_int.yarm
 
-public class Component {
+public abstract class Component {
   
   String name
   String normname
@@ -29,6 +29,31 @@ public class Component {
              name (nullable:true, blank:false, maxSize:2048)
          normname (nullable:true, blank:false, maxSize:2048)
         shortcode (nullable:true, blank:false, maxSize:128)
+  }
+
+  /**
+   *  Use rules particular to a resource type to either look up or create a DB record
+   *  based on the provided description.
+   */
+  def public static upsert(resource_description) {
+    throw new RuntimeException("Only concrete classes may upsert in this model");
+  }
+
+
+  /**
+   * Use properties from the resource_description to attempt to look up a record
+   * by value.
+   */
+  def public static lookupByValue(resource_description) {
+  }
+
+  /**
+   * The only hash method we know about at the component level is a hash of the normalised DC title
+   */
+  public Set getHashMethods() {
+    return [
+      [ 'dc_title' ]
+    ]
   }
 
 }
