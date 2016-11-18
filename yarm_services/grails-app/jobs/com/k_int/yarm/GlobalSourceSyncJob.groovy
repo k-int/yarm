4b23f05ef1579518b1108a3e38af59cc129ea46d
Ian Ibbotson <ian.ibbotson@k-int.com>
2016-11-18 16:07:03 +0000

    updated

---
+++ yarm_services/grails-app/jobs/com/k_int/yarm/GlobalSourceSyncJob.groovy
@@ -0,0 +1,22 @@
+package com.k_int.yarm
+
+class GlobalSourceSyncJob {
+
+  static triggers = {
+    // Every minute
+    cron name:'NewTenantWatcher', cronExpression: "0 0 3 * * ?"
+    // cronExpression: "s m h D M W Y"
+    //                  | | | | | | `- Year [optional]
+    //                  | | | | | `- Day of Week, 1-7 or SUN-SAT, ?
+    //                  | | | | `- Month, 1-12 or JAN-DEC
+    //                  | | | `- Day of Month, 1-31, ?
+    //                  | | `- Hour, 0-23
+    //                  | `- Minute, 0-59
+    //                  `- Second, 0-59
+  }
+
+  def execute() {
+    log.debug("GlobalSourceSyncJob");
+  }
+
+}
