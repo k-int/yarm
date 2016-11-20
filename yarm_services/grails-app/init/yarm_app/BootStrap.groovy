21dc7205a41ccfdc31f1ca01cf72dd7037251d8a
Ian Ibbotson <ianibbo@gmail.com>
2016-11-20 17:25:35 +0000

    Add gokb packages

--- yarm_services/grails-app/init/yarm_app/BootStrap.groovy
+++ yarm_services/grails-app/init/yarm_app/BootStrap.groovy
@@ -1,16 +1,25 @@
 package yarm_app
 
-class BootStrap {
+import com.k_int.yarm.*
 
-  def tenantManagerService
+class BootStrap {
 
   def init = { servletContext ->
     log.debug("BootStrap::init");
 
-    // Make sure we at least have a demo tenant
-    // N.B. Developers - add something like this to /etc/hosts so you can visit http://demo.localdomain/ and get access to the demo tenant
-    //   127.0.0.1       localhost pghost demo.localdomain
-    def demo_tenant = com.k_int.yarm.Org.findByUriName('demo') ?: new com.k_int.yarm.Org(uriName:'demo', displayName:'Demo').save(flush:true, failOnError:true);
+    def gokb_record_source = GlobalRecordSource.findByIdentifier('gokbPackages') ?: new GlobalRecordSource(
+                                                                                           identifier:'gokbPackages',
+                                                                                           name:'GOKB',
+                                                                                           type:'OAI',
+                                                                                           haveUpTo:null,
+                                                                                           uri:'https://gokb.kuali.org/gokb/oai/packages',
+                                                                                           listPrefix:'oai_dc',
+                                                                                           fullPrefix:'gokb',
+                                                                                           principal:null,
+                                                                                           credentials:null,
+                                                                                           rectype:0)
+    gokb_record_source.save(flush:true, stopOnError:true);
+    log.debug("New gokb record source: ${gokb_record_source}");
   }
 
   def destroy = {
--- yarm_services/grails-app/services/com/k_int/yarm/PackageLoadingService.groovy
+++ yarm_services/grails-app/services/com/k_int/yarm/PackageLoadingService.groovy
@@ -3,9 +3,26 @@ package com.k_int.yarm
 
 public class PackageLoadingService {
 
+  def running = false;
+
   @javax.annotation.PostConstruct
   def init() {
     log.debug("PackageLoadingService::init");
+  }
+
+  public void triggerSync() {
+    sychronized(this) {
+      if ( running == false ) {
+        running = true;
+        runSync()
+      }
+    }
+  }
+
+  public void runSync() {
+    log.debug("PackageLoadingService::runSync");
+
+    running = false;
   }
 
 }
