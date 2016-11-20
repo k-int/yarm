8043643336ae745af3b25d5d467948db32677d66
Ian Ibbotson <ianibbo@gmail.com>
2016-11-20 17:16:45 +0000

    Extend remote KB

--- yarm_dm/grails-app/domain/com/k_int/yarm/RemoteKB.groovy
+++ yarm_dm/grails-app/domain/com/k_int/yarm/RemoteKB.groovy
@@ -1,24 +1,47 @@
 package com.k_int.yarm
 
 public class RemoteKB {
-  
+
+  String identifier
   String name
-  String baseUrl
-  String profile
+  String type
+  Date haveUpTo
+  String uri
+  String listPrefix
+  String fullPrefix
+  String principal
+  String credentials
+  Long rectype
+  Boolean active
 
   static mapping = {
-    id column:'rkb_id'
-    version column:'rkb_version'
-    name column:'rkb_name', type:'text'
-    baseUrl column:'rkb_base_url', type:'text'
-    profile column:'rkb_profile', type:'text'
+                   id column:'rkb_id'
+              version column:'rkb_version'
+           identifier column:'rkb_identifier'
+                 name column:'rkb_name'
+             haveUpTo column:'rkb_have_up_to'
+                  uri column:'rkb_uri'
+           fullPrefix column:'rkb_full_prefix'
+           listPrefix column:'rkb_list_prefix'
+                 type column:'rkb_type'
+            principal column:'rkb_principal'
+          credentials column:'rkb_creds'
+              rectype column:'rkb_rectype'
+               active column:'rkb_active'
   }
-
 
   static constraints = {
-             name (nullable:true, blank:false, maxSize:2048)
-          baseUrl (nullable:true, blank:false, maxSize:2048)
-          profile (nullable:true, blank:false, maxSize:2048)
+     identifier(nullable:true, blank:false)
+           name(nullable:true, blank:false)
+       haveUpTo(nullable:true, blank:false)
+            uri(nullable:true, blank:false)
+           type(nullable:true, blank:false)
+     fullPrefix(nullable:true, blank:false)
+     listPrefix(nullable:true, blank:false)
+      principal(nullable:true, blank:false)
+    credentials(nullable:true, blank:false)
+         active(nullable:true, blank:false)
   }
+
 
 }
