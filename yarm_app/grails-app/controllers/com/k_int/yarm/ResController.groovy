081085ccfc18d353485d28ef04e26b0d3edecf4d
Ian Ibbotson <ian.ibbotson@gmail.com>
2017-03-03 11:04:18 +0000

    Fixed multiple transaction startup problem

---
+++ yarm_app/grails-app/controllers/com/k_int/yarm/ResController.groovy
@@ -0,0 +1,29 @@
+package com.k_int.yarm
+
+import grails.plugin.springsecurity.annotation.Secured
+import grails.converters.JSON
+
+
+class ResController {
+
+  def springSecurityService
+  def genericOIDService
+  def yarmConfigService
+
+  @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
+  def index() {
+
+    log.debug("ResourceController::index ${params} ${params.srch_cfg}");
+
+    def user = springSecurityService.currentUser
+    def result=[:]
+
+    log.debug("ResourceController::Index");
+    if (springSecurityService.isLoggedIn()){
+    }
+
+    log.debug("Returning..");
+    result;
+  }
+
+}
