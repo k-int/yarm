7fadeb38d68d00c46a354021186db10ab7c093ac
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-03-27 14:08:47 +0100

    add agreement.js

---
+++ yarm_app/grails-app/assets/javascripts/agreement.js
@@ -0,0 +1,32 @@
+
+console.log("agreement.js");
+
+if (typeof jQuery !== 'undefined') {
+  console.log("one");
+  (function($) {
+
+    console.log("two");
+
+    $("#findPkgBtn").click(
+      function(event)  {
+        var lookupUrl = $(this).data('lookup');
+        console.log("cleek url:%s",lookupUrl);
+        event.preventDefault();
+        $.ajax({
+          url: lookupUrl,
+          data:{
+              baseClass:'com.k_int.yarm.Package',
+              q: ''
+          }
+        }).done(function() {
+        });
+      }
+    );
+
+
+  })(jQuery);
+}
+
+function wibble() {
+  console.log("wibble");
+}
