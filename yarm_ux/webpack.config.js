fd4e18bd74035b8a9fe4203e725b8080daf33380
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-05-15 11:39:52 +0100

    More work on react demo

---
+++ yarm_ux/webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	  entry: './app/index.jsx',
+	  output: {
+		      path: path.resolve(__dirname, 'dist'),
+		      filename: 'index_bundle.js'
+		    },
+	  module: {
+		      rules: [
+			            { test: /\.(js)$/, use: 'babel-loader' },
+			            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]},
+									{ test: /\.(jsx)$/, use: 'babel-loader' }
+			          ]
+		    },
+	  plugins: [
+		      new HtmlWebpackPlugin({
+			            template: 'app/index.html'
+			          })
+		    ]
+};
