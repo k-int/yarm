100428a1a282938b53859e6d83d18a858e567f53
Ian Ibbotson <ian.ibbotson@k-int.com>
2017-05-22 12:43:48 +0100

    Workig on demo of jwt case based on redux-oauth-client-demo

--- yarm_ux/app/index.jsx
+++ yarm_ux/app/index.jsx
@@ -1,12 +1,45 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var PropTypes = require('prop-types');
+import React                                                      from 'react';
+import ReactDOM                                                   from 'react-dom';
+import PropTypes                                                  from 'prop-types';
+import { initialize, authStateReducer }                           from 'redux-oauth';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import thunk                                                      from 'redux-thunk';
+import { Provider }                                               from 'react-redux';
+
+import App                                                        from '../components/app';
 
 require('./index.css');
 
-import App from '../components/app.jsx';
+const store = createStore(
+  combineReducers({
+    auth: authStateReducer
+  }),
+  {},
+  compose(
+    applyMiddleware(thunk),
+    DevTools.instrument()
+  )
+);
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
+const reduxOauthConfig = {
+  backend: {
+    apiUrl:       'https://redux-oauth-backend.herokuapp.com',
+    signOutPath:  null,
+    authProviderPaths: {
+      github: '/auth/github'
+    }
+  },
+  cookies: document.cookie,
+  currentLocation: document.URL
+};
+
+store.dispatch(initialize(reduxOauthConfig)).then(
+  () => {
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.getElementById('app')
+    );
+  }
 );
