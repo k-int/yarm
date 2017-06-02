6fcaeb47774c6a52d2bfbc6977b31eed3d55b940
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2017-06-02 16:00:39 +0100

    Re jigged the API to include a "fetch" method that adds the necessary methods when dispatching the url call.

--- yarm_ux/utils/api.js
+++ yarm_ux/utils/api.js
@@ -1,40 +1,45 @@
 // var axios = require('axios');
 
 import { fetch as rofetch, parseResponse } from 'redux-oauth';
+import {dispatch, getState} from '../app/store';
 
-export function fetchUserProfile() {
-  console.log("yarm_api.fetchUserProfile()")
-  return { hello : 'hello' };
-}
+export default {
+	fetch : function () {
+		return rofetch.apply( null, arguments )(dispatch, getState);
+	},
+	
+	fetchUserProfile: function () {
+	  console.log("yarm_api.fetchUserProfile()")
+	  return { hello : 'hello' };
+	},
+	
+	ping: function () {
+	  console.log("yarm_api::Ping...");
+	  // Optionally the request above could also be done as
+	  // axios.get('http://localhost:8080/auth/ping', {
+	  var fr = fetch('http://localhost:8080/auth/ping', {
+	    // credentials: 'same-origin',  // 'include'
+	    credentials: 'include',
+	    params: {
+	      ID: 12345
+	    }
+	  });
 
-export function ping() {
-  console.log("yarm_api::Ping...");
-  // Optionally the request above could also be done as
-  // axios.get('http://localhost:8080/auth/ping', {
-  var fr = rofetch('http://localhost:8080/auth/ping', {
-    // credentials: 'same-origin',  // 'include'
-    credentials: 'include',
-    params: {
-      ID: 12345
-    }
-  });
+	  fr.then(( payload ) => console.log(payload));
 
-  fr.then(({ payload }) => console.log(payload));
+	  fr.catch(function (error) {
+	    console.log(error);
+	  });
 
-  fr.catch(function (error) {
-    console.log(error);
-  });
-
-  return "OK";
-}
-
-export function ping2() {
-  console.log("ping2");
-  // Ripped off from https://github.com/yury-dymov/redux-oauth-client-demo/blob/master/src/redux/actions.js
-  return dispatch => {
-    return dispatch(fetch('https://redux-oauth-backend.herokuapp.com/test/test'))
-      .then(parseResponse)
-      .then(({ payload }) => console.log("%o",payload))
-      .catch(errors => dispatch(apiError(errors)));
-  };
-}
+	  return "OK";
+	},
+	
+	ping2: function () {
+	  console.log("ping2");
+	  // Ripped off from https://github.com/yury-dymov/redux-oauth-client-demo/blob/master/src/redux/actions.js
+	  return fetch('https://redux-oauth-backend.herokuapp.com/test/test')
+	      .then(parseResponse)
+	      .then(({ payload }) => console.log("%o",payload))
+	      .catch(errors => console.log("Error",errors));
+	}
+};
